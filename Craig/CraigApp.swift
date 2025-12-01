ade8e17b9969a4cf5af41833878cf57ba0185ca5
Greg Rakozy <greg.rakozy@gmail.com>
2025-12-01 13:45:37 -0800

    Initial Commit

---
+++ Craig/CraigApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraigApp.swift
+//  Craig
+//
+//  Created by Greg Rakozy on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CraigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
